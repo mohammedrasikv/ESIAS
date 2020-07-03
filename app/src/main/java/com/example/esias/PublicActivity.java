3dac7b35eccdc1d195d651d81655c6478a4c5483
M Rasi KV <muhammedrazikv@gmail.com>
2020-07-03 16:20:07 +0530

    Project E Sias

---
+++ app/src/main/java/com/example/esias/PublicActivity.java
@@ -0,0 +1,14 @@
+package com.example.esias;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PublicActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_public);
+    }
+}
